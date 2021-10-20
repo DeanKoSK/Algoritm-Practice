516313d586fec9395f93c04282c3e657936700cb
DeanKoSK <kosunghan7209@gmail.com>
2021-10-19 23:59:44 -0600

    Better version

--- projectLargestPrimeFactor.js
+++ projectLargestPrimeFactor.js
@@ -5,7 +5,7 @@
 
 //logic 1: 프라임 팩터를 나오는 코드를 만들기
 //logic 2: 프라임 팩터들 중에 제일 큰 숫자 나오게 만들기
-//logic 3: 나누기가 안될떄 프라임 팩터를 하나씩 추가해주는 코드를 만들기
+//logic 3: 값이 안찾아질떄 primeFactor increment
 //logic 4: 그중에 제일 높은 프라임 팩터를 찾아내기.
 
 
@@ -15,8 +15,8 @@ function largestPrimeFactor(number) {
     for (let i = 0; i <= number; i++) {
         //유저가 부른 숫자만큼 도는 루프.
         if (number % primeFactor == 0) {
-
-
+ 
+        number /= primeFactor;
             // 여기에다 뭐 넣을지도 알아내야되고 
             // 여기에서 나오는 숫자중에 제일 큰 숫자를 찾아내는 코드도 만들어야되는데.
         } else {
@@ -30,3 +30,4 @@ function largestPrimeFactor(number) {
 }
 
 console.log(largestPrimeFactor(13195));
+// 13195부터 자꾸 틀리기 시작한다 by 1
